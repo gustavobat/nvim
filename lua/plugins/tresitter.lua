146fb8adf974ee7552a323416d117765aab50dd5
Gustavo <gustavoabatistela@gmail.com>
2023-04-12 09:59:57 -0300

    feat: add treesitter playground

--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -5,7 +5,10 @@ return { -- Highlight, edit, and navigate code
     build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-    dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/playground'
+    },
     config = function()
       require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
@@ -66,6 +69,24 @@ return { -- Highlight, edit, and navigate code
             },
           },
         },
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25,
+          persist_queries = false,
+          keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+          },
+        }
       }
     end
   }
