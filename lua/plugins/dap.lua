52e024e931b2da45374f3222bebda48f52d4bed2
Gustavo Batistela <gustavoabatistela@gmail.com>
2023-04-25 08:49:43 -0300

    add: initial dap config

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,48 @@
+-- DAP
+return {
+  'rcarriga/nvim-dap-ui',
+  dependencies = {
+    'mfussenegger/nvim-dap',
+    'theHamsta/nvim-dap-virtual-text',
+    'jbyuki/one-small-step-for-vimkind',
+  },
+  config = function()
+    local dap = require('dap')
+    local dapui = require('dapui')
+    dapui.setup()
+    require('nvim-dap-virtual-text').setup({})
+    -- Looks
+    local terafox_red = require('nightfox.palette').load('terafox').red.base
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = terafox_red })
+    vim.fn.sign_define('DapBreakpoint', { text = '⏺', texthl = 'DapBreakpoint' })
+    vim.fn.sign_define('DapStopped', { text = '⏸' })
+    -- Keymaps
+    vim.api.nvim_set_keymap('n', '<F8>', [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true })
+    vim.api.nvim_set_keymap('n', '<F9>', [[:lua require"dap".continue()<CR>]], { noremap = true })
+    vim.api.nvim_set_keymap('n', '<F10>', [[:lua require"dap".step_over()<CR>]], { noremap = true })
+    vim.api.nvim_set_keymap('n', '<S-F10>', [[:lua require"dap".step_into()<CR>]], { noremap = true })
+    vim.api.nvim_set_keymap('n', '<F12>', [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true })
+    vim.api.nvim_set_keymap('n', '<F5>', [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true })
+    -- language specific
+    dap.configurations.lua = {
+      {
+        type = 'nlua',
+        request = 'attach',
+        name = "Attach to running Neovim instance",
+      }
+    }
+
+    dap.adapters.nlua = function(callback, config)
+      callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
+    end
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+  end
+}
