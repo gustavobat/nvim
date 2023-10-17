87ea7aa120b177311cbe12acbec0db8b9ad22445
Gustavo <gustavoabatistela@gmail.com>
2023-10-17 10:03:07 -0300

    chore: upgrade telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.1',
+  tag = '0.1.4',
   dependencies = { { 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-live-grep-args.nvim' } },
   config = function()
     require('telescope').setup()
